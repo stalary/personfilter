88b45f263452ca7b49a74ba4b8744c85f286ef10
stalary <452024236@qq.com>
2018-04-09 16:59:57 +0800

    INIT:完成测试类

--- src/main/java/com/stalary/personfilter/PersonfilterApplication.java
+++ src/main/java/com/stalary/personfilter/PersonfilterApplication.java
@@ -2,11 +2,59 @@ package com.stalary.personfilter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@Configuration
+@EnableSwagger2
+@EnableTransactionManagement
 public class PersonfilterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PersonfilterApplication.class, args);
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .genericModelSubstitutes(DeferredResult.class)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.stalary.personfilter.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("人才筛选")
+                .description("源码请访问：https://github.com/stalary/personfilter")
+                .termsOfServiceUrl("stalary.com")
+                .version("1.0")
+                .build();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
     }
 }
