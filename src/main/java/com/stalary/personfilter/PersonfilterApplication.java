d5db9e925632530ae581c53cbe22dfc9d0fbcbc0
stalary <452024236@qq.com>
2018-04-09 16:22:50 +0800

    INIT

---
+++ src/main/java/com/stalary/personfilter/PersonfilterApplication.java
@@ -0,0 +1,12 @@
+package com.stalary.personfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonfilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonfilterApplication.class, args);
+    }
+}
---
+++ src/test/java/com/stalary/personfilter/PersonfilterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stalary.personfilter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonfilterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
