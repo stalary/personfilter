5220dd30eff362b165a8aba0036f48248ab4d797
stalary <452024236@qq.com>
2018-04-16 00:17:30 +0800

    ADD: 完成登录注册

---
+++ src/main/java/com/stalary/personfilter/repo/mongodb/ResumeRepo.java
@@ -0,0 +1,12 @@
+package com.stalary.personfilter.repo.mongodb;
+
+import com.stalary.personfilter.data.entity.mongodb.Resume;
+
+/**
+ * @author Stalary
+ * @description
+ * @date 2018/4/13
+ */
+public interface ResumeRepo extends BaseRepo<Resume, Long> {
+
+}
