41cf624ea7ea0c47d35a9cfb354b7907c1934609
stalary <452024236@qq.com>
2018-06-06 20:31:24 +0800

    MOD

--- src/main/java/com/stalary/personfilter/controller/CommonController.java
+++ src/main/java/com/stalary/personfilter/controller/CommonController.java
@@ -4,6 +4,7 @@ import com.stalary.personfilter.data.vo.ResponseMessage;
 import com.stalary.personfilter.service.outer.SmsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
